a07c141eefe1aafa1c034ea0626a448d2ab8a84c
Vuyo Mathe <88540323+DarthNola@users.noreply.github.com>
2024-08-06 18:01:16 +0200

    Created react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Generator from './components/Generator'
+import Hero from './components/Hero'
+import Workout from './components/Workout'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <Hero/>
+      <Generator/>
+      <Workout/>
+
     </>
   )
 }
